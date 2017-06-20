6a2fbc1d9c1bafe1ade73897ad2200fd6798ed6e
Scott <marden.scott@gmail.com>
2017-06-20 14:42:22 -0700

    Minor refactoring of code and added some clarifying comments to views.py

--- apps/products_app/views.py
+++ apps/products_app/views.py
@@ -6,16 +6,16 @@ from .models import Product
 
 # Create your views here.
 def index(request):
-	# Product.objects.create(name="Campbells Tomato Soup", description="A delicious can of the tomato soup you know and love!", weight="8oz", price=2.00, cost=.50, category="Canned Foods")
-	# Product.objects.create(name="Pirate's Booty", description="A fun twist on cheese puffs!", weight="16oz", price=4.99, cost=1.25, category="Snacks")
-	# Product.objects.create(name="Macbook Pro", description="A reliable, powerful, overpriced status symbol that you can use to check your facebook", weight="3.02lbs", price=2000.00, cost=14.50, category="Luxury Items")
+	#Commenting out adding of products, so that it doesn't reload on every page refresh. Can also do this in the python shell
+		# Product.objects.create(name="Campbells Tomato Soup", description="A delicious can of the tomato soup you know and love!", weight="8oz", price=2.00, cost=.50, category="Canned Foods")
+
+		# Product.objects.create(name="Pirate's Booty", description="A fun twist on cheese puffs!", weight="16oz", price=4.99, cost=1.25, category="Snacks")
+
+		# Product.objects.create(name="Macbook Pro", description="A reliable, powerful, overpriced status symbol that you can use to check your facebook", weight="3.02lbs", price=2000.00, cost=14.50, category="Luxury Items")
+		
+	#print info about products
 	products = Product.objects.all()
 	print products
 	for product in products:
-		print product.name
-		print product.description
-		print product.weight
-		print product.price
-		print product.cost
-		print product.category
+		print product.name, product.description, product.weight, product.price, product.cost, product.category
 	return render(request, 'products_app/index.html')
